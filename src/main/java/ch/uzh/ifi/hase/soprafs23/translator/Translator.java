9b63caf34ece77e4564f1d075a349369a339f245
LeoEngelberger <87647723+LeoEngelberger@users.noreply.github.com>
2023-04-01 17:49:10 +0200

    Setup Translator
    Setup necessary:
    
    --Install Google Cloud Code to inteliJ, VSCode.
    --In the Tools menu, select Cloud Code > Cloud APIs
    --sign-in to Google Cloud
    --setup GOOGLE_APPLICATION_CREDENTIALS; (possible this does not yet work on the server and to get it working locally auth setup required)
    
    Add:
    -Translator Class
    -Translator Test
    -Google Cloud Translate Key

---
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/translator/Translator.java
@@ -0,0 +1,57 @@
+package ch.uzh.ifi.hase.soprafs23.translator;
+// Imports the Google Cloud Translation library.
+
+import com.google.cloud.translate.v3.*;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Translator {
+    static String projectId = "sopra-fs23-group-25-server";
+    static LocationName parent = LocationName.of(projectId, "global");
+
+    private static TranslateTextResponse translateText(String sourceLanguage, String word, TranslationServiceClient client) throws IOException {
+
+        // Supported Mime Types: https://cloud.google.com/translate/docs/supported-formats
+        TranslateTextRequest request = TranslateTextRequest.newBuilder().setParent(parent.toString()).setMimeType("text/plain").setSourceLanguageCode(sourceLanguage).setTargetLanguageCode("en").addContents(word).build();
+
+        return client.translateText(request);
+    }
+
+    public String translateSingle(String word, String language) throws IOException {
+        // Initialize client that will be used to send requests. This client only needs to be created
+        // once, and can be reused for multiple requests. using it in a try statement should ensure automatic close call, need to see if this is a problem
+        String translationText = "";
+        try (TranslationServiceClient client = TranslationServiceClient.create()) {
+            TranslateTextResponse response = translateText(language, word, client);
+            for (Translation translation : response.getTranslationsList()) {
+                translationText = translation.getTranslatedText();
+            }
+        }
+        catch (IOException e) {
+            System.out.print(e.getMessage());
+        }
+        return translationText;
+    }
+
+    public List<String> translateList(List<String> wordList, String language) throws IOException {
+        // Initialize client that will be used to send requests. This client only needs to be created
+        // once, and can be reused for multiple requests. using it in a try statement should ensure automatic close call, need to see if this is a problem
+        List<String> translatedStrings = new ArrayList<String>();
+        try (TranslationServiceClient client = TranslationServiceClient.create()) {
+            for (String word : wordList) {
+                TranslateTextResponse response = translateText(language, word, client);
+                for (Translation translation : response.getTranslationsList()) {
+                    translatedStrings.add(translation.getTranslatedText());
+
+                }
+            }
+        }
+        catch (IOException e) {
+            System.out.print(e.getMessage());
+        }
+        return translatedStrings;
+    }
+}
+
