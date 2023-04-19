46718fb81c6f1c5463659a2ec75e5fe69f32746a
jo-ana-c <joana.cieri@uzh.ch>
2023-04-19 13:39:15 +0200

    rename

--- src/main/java/ch/uzh/ifi/hase/soprafs23/rest/mapper/DTOMapper.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/rest/mapper/DTOMapper.java
@@ -55,13 +55,13 @@ public interface DTOMapper {
     @Mapping(source = "lobbyId", target = "lobbyId")
     @Mapping(source = "lobbyName", target = "lobbyName")
     @Mapping(source = "nrOfRounds", target = "nrOfRounds")
-    @Mapping(source = "numberOfPlayers", target = "numberOfPlayers")
+    @Mapping(source = "nrOfPlayers", target = "nrOfPlayers")
     @Mapping(source = "timePerRound", target = "timePerRound")
     LobbyGetDTO convertEntityToLobbiesGetDTO(Lobby lobby);
 
     @Mapping(source = "lobbyName", target = "lobbyName")
     @Mapping(source = "nrOfRounds", target = "nrOfRounds")
-    @Mapping(source = "numberOfPlayers", target = "numberOfPlayers")
+    @Mapping(source = "nrOfPlayers", target = "nrOfPlayers")
     @Mapping(source = "timePerRound", target = "timePerRound")
     @Mapping(source = "hostId", target = "hostId")
     Lobby convertLobbiesPostDTOToEntity(LobbyPostDTO newLobby);
