ee36e388a2113c363f93742544ffd06ff8ddcc2b
LeoEngelberger <87647723+LeoEngelberger@users.noreply.github.com>
2023-04-23 14:27:41 +0200

    Added new Functionality for Websockets

---
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/websockets/dto/UserSocketGetDTO.java
@@ -0,0 +1,15 @@
+package ch.uzh.ifi.hase.soprafs23.websockets.dto;
+
+public class UserSocketGetDTO {
+    private String username;
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+
+
+}
