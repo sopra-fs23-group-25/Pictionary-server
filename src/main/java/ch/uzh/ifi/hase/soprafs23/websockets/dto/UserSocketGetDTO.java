7fd02da091b62003508974bb9607eb42f67d81c2
jo-ana-c <joana.cieri@uzh.ch>
2023-04-26 10:32:30 +0200

    80% overall coverage, cleanup

--- src/main/java/ch/uzh/ifi/hase/soprafs23/websockets/dto/UserSocketGetDTO.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/websockets/dto/UserSocketGetDTO.java
@@ -2,13 +2,17 @@ package ch.uzh.ifi.hase.soprafs23.websockets.dto;
 
 public class UserSocketGetDTO {
     private String username;
+    private long userId;
+
     public String getUsername() {
         return username;
     }
-
     public void setUsername(String username) {
         this.username = username;
     }
+
+    public void setUserId (long userId) {this.userId = userId;}
+    public long getUserId() {return userId;}
 
 
 
