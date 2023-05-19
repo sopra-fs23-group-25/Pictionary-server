5bab584d8c352ebf9c444161d2eb5b8e23957012
jo-ana-c <joana.cieri@uzh.ch>
2023-05-19 10:49:56 +0200

    #246

--- src/main/java/ch/uzh/ifi/hase/soprafs23/wordassigner/WordAssigner.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/wordassigner/WordAssigner.java
@@ -16,7 +16,7 @@ public class WordAssigner {
     private final LobbyRepository lobbyRepository;
     private final List<String> possibleWords = Arrays.asList("fish", "dog", "duck", "music", "snail", "mountain",
             "moon", "tree", "rainbow", "pizza", "flower", "zoo", "king", "sun", "rocket", "lamp", "clock", "rain",
-            "milk", "carrot", "spoon", "nose", "snowman", "cheese");
+            "milk", "carrot", "spoon", "nose", "snowman", "cheese", "snowflake", "bus", "candle");
     private final Random random;
 
     @Autowired
